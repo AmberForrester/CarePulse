0163eb595d158d874a73da8298faaac29e2701ba
Amber <157823790+AmberForrester@users.noreply.github.com>
2024-10-31 10:04:21 -0400

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 
 import { cn } from "@/lib/utils";
@@ -23,7 +24,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="dark">
+            {children}
+          </ThemeProvider>
+        </body>
     </html>
   );
 }
